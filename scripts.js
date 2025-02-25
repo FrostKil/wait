15ced8c3b830aab5ad36365ce82476a1b16c5eca
FrostKil <frostalshboul@gmail.com>
2025-02-25 04:10:29 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+if (sessionStorage.getItem("asked")) {
+    document.getElementById("question").innerText = "I already asked you";
+    document.querySelectorAll("button").forEach(btn => btn.style.display = "none");
+}
+
+function answer(response) {
+    document.getElementById("yesText").style.display = response === "yes" ? "block" : "none";
+    document.getElementById("noText").style.display = response === "no" ? "block" : "none";
+    document.getElementById("wow").style.display = response === "no" ? "block" : "none";
+    document.querySelectorAll("button").forEach(btn => btn.disabled = true);
+
+    sessionStorage.setItem("asked", true);
+
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 2000);
+}
+fetch('https://ipinfo.io/json?token=8f61dbdc04dbe0')
+.then(response => response.json())
+.then(data => {
+document.getElementById("noText").innerText = 
+     data.city + ', ' + data.region + ', ' + data.country;
+})
